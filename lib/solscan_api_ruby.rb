5103a7faa326acd02d491625e210d07790d0ab3e
Radoslaw Stolarski <radoslaw.stolarski@polcode.net>
2022-01-25 15:19:28 +0100

    Set api_url

---
+++ lib/solscan_api_ruby.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module SolscanApiRuby
+  class << self
+    # Api url.
+    # @return [String] api url.
+    def api_url
+      "https://public-api.solscan.io/"
+    end
+  end
+end
